d6c4bd86c156c8148b75cd02b38cd097a20e3c3f
Nahuel Nievas <nahuelnievas30@gmail.com>
2026-04-24 20:54:45 -0300

    feat: fixes mapa

--- src/view/MapaView.java
+++ src/view/MapaView.java
@@ -13,6 +13,8 @@ import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JList;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MapaView extends JFrame {
 
@@ -34,24 +36,30 @@ public class MapaView extends JFrame {
 		getContentPane().add(mapa, BorderLayout.CENTER);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(533, 78, 249, 351);
+		panel.setBounds(522, 78, 260, 430);
 		mapa.add(panel);
 		panel.setLayout(null);
 		
 		JButton btnAddLocalidad = new JButton("Agregar Localidad");
-		btnAddLocalidad.setBounds(10, 10, 123, 42);
+		btnAddLocalidad.setToolTipText("Agregar una localidad al mapa");
+		btnAddLocalidad.setFont(new Font("SansSerif", Font.BOLD, 10));
+		btnAddLocalidad.setBounds(10, 10, 132, 42);
 		panel.add(btnAddLocalidad);
 		
 		JButton btnCalcular = new JButton("Calcular");
-		btnCalcular.setBounds(143, 10, 96, 42);
+		btnCalcular.setToolTipText("Calcular el costo de transporte entre localidades");
+		btnCalcular.setFont(new Font("SansSerif", Font.BOLD, 10));
+		btnCalcular.setBounds(168, 10, 79, 42);
 		panel.add(btnCalcular);
 		
 		JButton btnCleanMapa = new JButton("Limpiar Mapa");
-		btnCleanMapa.setBounds(143, 62, 96, 42);
+		btnCleanMapa.setToolTipText("Limpiar el mapa de localidades");
+		btnCleanMapa.setFont(new Font("SansSerif", Font.BOLD, 10));
+		btnCleanMapa.setBounds(125, 377, 122, 42);
 		panel.add(btnCleanMapa);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 105, 123, 108);
+		scrollPane.setBounds(10, 114, 142, 198);
 		panel.add(scrollPane);
 		
 		JList list = new JList();
@@ -60,37 +68,58 @@ public class MapaView extends JFrame {
 		JLabel lblNewLabel = new JLabel("Localidades");
 		lblNewLabel.setFont(new Font("SansSerif", Font.BOLD, 12));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(20, 62, 85, 42);
+		lblNewLabel.setBounds(33, 75, 85, 42);
 		panel.add(lblNewLabel);
 		
 		txtCostoKm = new JTextField();
 		txtCostoKm.setText("0");
-		txtCostoKm.setBounds(10, 270, 96, 18);
+		txtCostoKm.setToolTipText("Costo por kilómetro de transporte");
+		txtCostoKm.setFont(new Font("SansSerif", Font.PLAIN, 11));
+		txtCostoKm.setBounds(10, 336, 96, 18);
 		panel.add(txtCostoKm);
 		txtCostoKm.setColumns(10);
 		
 		txtRecargo = new JTextField();
 		txtRecargo.setText("0");
-		txtRecargo.setBounds(143, 270, 96, 18);
+		txtRecargo.setToolTipText("Porcentaje de recargo por transporte urgente");
+		txtRecargo.setFont(new Font("SansSerif", Font.PLAIN, 11));
+		txtRecargo.setBounds(129, 336, 96, 18);
 		panel.add(txtRecargo);
 		txtRecargo.setColumns(10);
 		
 		txtCostoDifProv = new JTextField();
 		txtCostoDifProv.setText("0");
-		txtCostoDifProv.setBounds(10, 323, 96, 18);
+		txtCostoDifProv.setToolTipText("Costo adicional por transportar entre diferentes provincias");
+		txtCostoDifProv.setFont(new Font("SansSerif", Font.PLAIN, 11));
+		txtCostoDifProv.setBounds(10, 377, 96, 18);
 		panel.add(txtCostoDifProv);
 		txtCostoDifProv.setColumns(10);
 		
-		JLabel lblNewLabel_1 = new JLabel("Costo x KM");
-		lblNewLabel_1.setBounds(10, 251, 95, 18);
-		panel.add(lblNewLabel_1);
+		JLabel lblCostoXKm = new JLabel("Costo x KM");
+		lblCostoXKm.setFont(new Font("SansSerif", Font.PLAIN, 11));
+		lblCostoXKm.setBounds(10, 315, 95, 18);
+		panel.add(lblCostoXKm);
 		
-		JLabel lblNewLabel_2 = new JLabel("Costo por diferentes provincias");
-		lblNewLabel_2.setBounds(10, 298, 190, 26);
-		panel.add(lblNewLabel_2);
+		JLabel lblCostoDifProv = new JLabel("Costo por diferentes provincias");
+		lblCostoDifProv.setFont(new Font("SansSerif", Font.PLAIN, 11));
+		lblCostoDifProv.setBounds(10, 354, 190, 26);
+		panel.add(lblCostoDifProv);
 		
-		JLabel lblNewLabel_3 = new JLabel("% de recargo");
-		lblNewLabel_3.setBounds(143, 252, 96, 16);
-		panel.add(lblNewLabel_3);
+		JLabel lblPorcRecargo = new JLabel("% de recargo");
+		lblPorcRecargo.setFont(new Font("SansSerif", Font.PLAIN, 11));
+		lblPorcRecargo.setBounds(129, 316, 96, 16);
+		panel.add(lblPorcRecargo);
+		
+		JButton btnUpdateLocalidad = new JButton("Actualizar");
+		btnUpdateLocalidad.setToolTipText("Actualizar la información de una localidad seleccionada en la lista");
+		btnUpdateLocalidad.setFont(new Font("SansSerif", Font.BOLD, 10));
+		btnUpdateLocalidad.setBounds(162, 135, 85, 42);
+		panel.add(btnUpdateLocalidad);
+		
+		JButton btnDeleteLocalidad = new JButton("Eliminar ");
+		btnDeleteLocalidad.setToolTipText("Eliminar una localidad seleccionada de la lista");
+		btnDeleteLocalidad.setFont(new Font("SansSerif", Font.BOLD, 10));
+		btnDeleteLocalidad.setBounds(162, 183, 85, 42);
+		panel.add(btnDeleteLocalidad);
 	}
 }
